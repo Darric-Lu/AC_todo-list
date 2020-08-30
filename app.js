206abce6edf4ff7519a87473799ef4415dd0eaf1
Darric <darric.lu@gmail.com>
2020-08-31 00:06:22 +0800

    Feat: project init

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello welcome')
+})
+app.listen(port, () => {
+  console.log('App is listening on http://localhost:3000')
+})
