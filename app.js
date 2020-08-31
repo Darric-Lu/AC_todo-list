2baedf1d9da9f642fa188f6192c095b57122408a
Darric <darric.lu@gmail.com>
2020-08-31 21:04:06 +0800

    Add mongoose

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('mongoose error!')
+})
+db.once('open', () => {
+  console.log('mongoose connected!')
+})
 
 app.get('/', (req, res) => {
   res.send('Hello welcome')
 })
-app.listen(port, () => {
+app.listen(3000, () => {
   console.log('App is listening on http://localhost:3000')
 })
