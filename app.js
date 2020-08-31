e818896d1af3656f9e58ccefef1dcf48ee4e694a
Darric <darric.lu@gmail.com>
2020-08-31 22:13:13 +0800

    Modify index.hbs

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express()
 const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
 const exphbs = require('express-handlebars')
+const Todo = require('./models/todo')
 
 const db = mongoose.connection
 db.on('error', () => {
@@ -15,7 +16,10 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 app.get('/', (req, res) => {
-  res.render('index')
+  Todo.find()
+    .lean()
+    .then(todos => res.render('index', { todos }))
+    .catch(error => console.error(error))
 })
 app.listen(3000, () => {
   console.log('App is listening on http://localhost:3000')
