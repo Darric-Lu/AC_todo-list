8caa61f3c2fdbfdbc312ae1cc15ea08186ed50fb
Darric <darric.lu@gmail.com>
2020-08-31 21:49:46 +0800

    add express-handlebars

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
+const exphbs = require('express-handlebars')
 
 const db = mongoose.connection
 db.on('error', () => {
@@ -10,9 +11,11 @@ db.on('error', () => {
 db.once('open', () => {
   console.log('mongoose connected!')
 })
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
 
 app.get('/', (req, res) => {
-  res.send('Hello welcome')
+  res.render('index')
 })
 app.listen(3000, () => {
   console.log('App is listening on http://localhost:3000')
